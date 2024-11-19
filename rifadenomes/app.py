8ed597302c6fb2f2a804348116116bfe12c3a556
Alex Sada <140125254+thealexsada@users.noreply.github.com>
2024-11-19 06:40:51 +0000

    Tue, Nov 19, 2024, 7:40 PM +13:00

--- rifadenomes/app.py
+++ rifadenomes/app.py
@@ -45,6 +45,11 @@ def register():
     # Confirm registration
     return redirect("/registrants")
 
+@app.route("/registrants")
+def registrants():
+    registrants = db.execute("SELECT * FROM registrants")
+    return render_template("registrants.html", registrants=registrants)
+
 
 @app.route('/nomes', methods=['GET'])
 def nomes():
